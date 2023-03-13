c2db2f213cadd865e13d5bdd17202015285a2ba4
Charles Badibanga <charlesbadibanga2000@yahoo.fr>
2023-03-12 20:22:53 -0400

    This is a complete spring Boot project with no database connected.

---
+++ src/main/java/com/genspark/SpringBootDemoApp/Controller/MyController.java
@@ -0,0 +1,45 @@
+package com.genspark.SpringBootDemoApp.Controller;
+
+import com.genspark.SpringBootDemoApp.Entity.Course;
+import com.genspark.SpringBootDemoApp.Service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class MyController {
+
+    @Autowired
+    private CourseService CourseService;
+
+    @GetMapping("/")
+    public String home() {
+        return "<HTML><H1>Welcome to Course Application</H1></HTML>";
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getCourses(){
+        return this.CourseService.getAllCourse();
+    }
+
+    @GetMapping("/courses/{courseID}")
+    private Course getCourse(@PathVariable String courseID) {
+        return this.CourseService.getCourseById(Integer.parseInt(courseID));
+    }
+
+    @PostMapping("/courses")
+    public Course addCourse(@RequestBody Course course) {
+        return this.CourseService.addCourse(course);
+    }
+
+    @PutMapping("/courses")
+    public Course updateCourse(@RequestBody Course course) {
+        return this.CourseService.updateCourse(course);
+    }
+
+    @DeleteMapping("/courses/{courseID}")
+    private String deleteCourse(@PathVariable String courseID) {
+        return this.CourseService.deleteCourseById(Integer.parseInt(courseID));
+    }
+}
